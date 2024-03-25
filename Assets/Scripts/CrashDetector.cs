e93a813e8904697aa0b8a4645461f4656cd7b6e1
hensu0210 <57539464+hensu0210@users.noreply.github.com>
2024-03-25 16:54:33 +0900

    Enviroment setting
    enviroment set

--- Assets/Scripts/CrashDetector.cs
+++ Assets/Scripts/CrashDetector.cs
@@ -6,11 +6,13 @@ using UnityEngine.SceneManagement;
 public class CrashDetector : MonoBehaviour
 {
     [SerializeField] float loadDelay = 0.5f;
+    [SerializeField] ParticleSystem crashEffect;
 
     void OnTriggerEnter2D(Collider2D collision)
     {
         if(collision.tag == "Ground")
         {
+            crashEffect.Play();
             Invoke(nameof(ReloadScene), loadDelay);
         }
     }
--- Assets/Scripts/FinishLine.cs
+++ Assets/Scripts/FinishLine.cs
@@ -6,11 +6,13 @@ using UnityEngine.SceneManagement;
 public class FinishLine : MonoBehaviour
 {
     [SerializeField] float loadDelay = 1f;
+    [SerializeField] ParticleSystem finishEffect;
 
     void OnTriggerEnter2D(Collider2D collision)
     {
         if(collision.tag == "Player")
         {
+            finishEffect.Play();
             Invoke(nameof(ReloadScene), loadDelay);
         }
     }
